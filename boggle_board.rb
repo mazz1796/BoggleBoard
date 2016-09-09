cead69aec112f8f390c354906bb69397d47c3aa4
Koji Hiramatsu <mazz1796@gmail.com>
2016-09-09 15:46:52 +0800

    Boggle

--- boggle_board.rb
+++ boggle_board.rb
@@ -1,20 +1,45 @@
 class BoggleBoard
+
   def initialize
+    @board = Array.new(4){Array.new(4)}  #creat an empty nested array with 4 elements each of the level
+ 
   end
   
   def shake!
+    
+    @board.each_with_index do |x, xindex|  #repeat the block the number of elememts.
+      x.each_with_index do |y, yindex|    #
+        @board[xindex][yindex]= ('A'..'Z').to_a.sample
+        if @board[xindex][yindex] == 'Q' 
+            @board[xindex][yindex] = 'Qu'
+        end 
+        
+      end
+    end
+
+ 
   end
   
-  # Defining to_s on an object controls how the object is
-  # represented as a string, e.g., when you pass it to puts
-  #
-  # Override this to print out a sensible board format so
-  # you can write code like:
-  #
-  # board = BoggleBoard.new
-  # board.shake!
-  # puts board
-  def to_s
-    "omg what is this? try printing me."
+
+  def join
+    @board.each do |x|
+      puts "#{x[0].ljust(3)}#{x[1].ljust(3)}#{x[2].ljust(3)}#{x[3].ljust(3)} "   #ljustはStringのみに使えるmethod
+      # board.join: combine elements in a array into one string. in this case, apply for only x because we need 4 sets of 4 letters.
+    end
+
   end
 end
+
+# instance method, means they only run on instance variable
+
+board = BoggleBoard.new #create one boggle board object #initializeをするとき、ここでBoggleBoardオブジェクトをつくることが超重要。
+
+board.shake!
+board.join #what is this doing? 
+
+puts "----------"
+board.shake!
+board.join
+
+# .split changes string to Array
+# .join changes array to string
